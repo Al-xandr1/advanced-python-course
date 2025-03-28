784dd36870bf5cba0f64b7ca3e78a2e0d2cf75d0
xandr <al-xandr1@yandex.ru>
2025-03-29 00:16:47 +0400

    first commit

---
+++ easy.py
@@ -0,0 +1,13 @@
+import math
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a / b)
+    print(a // b)
+    print(a % b)
+    print(math.sqrt(a ** 10 + b ** 10))
